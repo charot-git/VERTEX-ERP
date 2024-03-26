edf6af099660e952b3697ca2c812b227ea86ff61
Andrei Siapno <gamingandrei06@gmail.com>
2024-03-26 21:55:13 +0800

    Credit Debit Memo UI Configuration for server management

--- src/main/java/com/vertex/vos/Constructors/DatabaseConfig.java
+++ src/main/java/com/vertex/vos/Constructors/DatabaseConfig.java
@@ -1,7 +1,7 @@
 package com.vertex.vos.Constructors;
 
 public class DatabaseConfig {
-    public static final String DATABASE_URL = "jdbc:mysql://25.7.130.79:3309/";
+    public static final String DATABASE_URL = "jdbc:mysql://192.168.5.227:3306/";
     public static final String DATABASE_USERNAME = "developmentUser";
     public static final String DATABASE_PASSWORD = "developer";
 }
