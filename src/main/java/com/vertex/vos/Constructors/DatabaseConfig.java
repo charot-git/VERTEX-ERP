30663ea40b4ddb9e4714098d7b5042628637d10d
Andrei Siapno <gamingandrei06@gmail.com>
2024-04-30 12:09:06 +0800

    receiving updates

--- src/main/java/com/vertex/vos/Constructors/DatabaseConfig.java
+++ src/main/java/com/vertex/vos/Constructors/DatabaseConfig.java
@@ -1,7 +1,25 @@
 package com.vertex.vos.Constructors;
 
 public class DatabaseConfig {
-    public static final String DATABASE_URL = "jdbc:mysql://Development:3306/";
-    public static final String DATABASE_USERNAME = "vosSystem";
-    public static final String DATABASE_PASSWORD = "Meneses81617VOS";
+    public static final String DATABASE_URL;
+    public static final String DATABASE_USERNAME;
+    public static final String DATABASE_PASSWORD;
+
+    // Toggle between configurations based on the environment
+    static {
+        String environment = "development"; // Set this dynamically based on your environment
+        switch (environment) {
+            case "development":
+                DATABASE_URL = "jdbc:mysql://Development:3306/";
+                DATABASE_USERNAME = "vosSystem";
+                DATABASE_PASSWORD = "Meneses81617VOS";
+                break;
+            case "production":
+            default:
+                DATABASE_URL = "jdbc:mysql://192.168.1.49:3309/";
+                DATABASE_USERNAME = "vosSystem";
+                DATABASE_PASSWORD = "Meneses81617VOS";
+                break;
+        }
+    }
 }
