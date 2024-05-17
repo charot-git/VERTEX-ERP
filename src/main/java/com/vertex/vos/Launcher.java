7020e3a209d03a164cbbd8dcad314ce577dbfff1
Andrei Siapno <gamingandrei06@gmail.com>
2024-05-17 11:35:50 +0800

    Product UX Updates Launch Optimizations - Loading Screen Implemetation Patched version for live done.

--- src/main/java/com/vertex/vos/Launcher.java
+++ src/main/java/com/vertex/vos/Launcher.java
@@ -1,11 +1,37 @@
 package com.vertex.vos;
 
+import com.vertex.vos.Utilities.DialogUtils;
 import com.vertex.vos.Utilities.LocationCache;
 import javafx.application.Application;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Launcher {
-    public static void main(String[]args){
+    private static final String LOCK_FILE_PATH = System.getProperty("user.home") + File.separator + ".vos.lock";
+
+    public static void main(String[] args) {
+        if (isAppAlreadyRunning()) {
+            DialogUtils.showErrorMessage("Error", "Another instance of the application is already running.");
+            return;
+        }
+
         LocationCache.initialize();
         Application.launch(Main.class);
+    }
+
+    private static boolean isAppAlreadyRunning() {
+        File lockFile = new File(LOCK_FILE_PATH);
+        try {
+            if (lockFile.exists()) {
+                return true;
+            } else {
+                lockFile.createNewFile();
+                lockFile.deleteOnExit(); // Delete lock file when JVM exits
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating lock file: " + e.getMessage());
+        }
+        return false;
     }
 }
